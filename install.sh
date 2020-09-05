243b84d6469ba4ad9ca9e1e823a22f1d9f71cd93
xxooomg <44080839+xxooomg@users.noreply.github.com>
2020-09-05 14:23:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -96,7 +96,7 @@ install(){
     echo "进行安装。。。"
     cd ${SH_PATH}/IBMYes/v2ray-cloudfoundry
     ibmcloud target --cf
-    echo "Y"|ibmcloud cf install
+    echo "y"|ibmcloud cf install
     ibmcloud cf push
     echo "安装完成。"
     echo "生成的随机 UUID：${UUID}"
